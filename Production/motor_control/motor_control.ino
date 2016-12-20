f7de7cb2788b0248b6e18b3e598bab95b7b87c92
zhengqixi <zhengqixi@github.com>
2016-12-20 16:29:09 -0500

    So modular...

--- Production/motor_control/motor_control.ino
+++ Production/motor_control/motor_control.ino
@@ -1,18 +1,21 @@
 #include<Adafruit_MotorShield.h>
 #include<Wire.h>
-#define motor_front_port 1
-#define motor_back_port 2
-#define wire_port 8
+#define motor_front_port 3
+#define motor_back_port 1
+#define wire_port 0x20
 #define default_speed 255
+#define fire_pin 6
 
-Adafruit_MotorShield shield = Adafruit_MotorShield();
+Adafruit_MotorShield shield = Adafruit_MotorShield(0x60);
 Adafruit_DCMotor* motor_front = shield.getMotor(motor_front_port);
 Adafruit_DCMotor* motor_back = shield.getMotor(motor_back_port);
 
-void forward(int duration);
-void backward(int duration);
-void right(int duration);
-void left(int duration);
+void forward();
+void backward();
+void right();
+void left();
+void brake();
+void fire();
 
 void setup() {
   shield.begin();
@@ -25,28 +28,61 @@ void setup() {
   motor_back->run(FORWARD);
   motor_back->run(RELEASE);
 
-  Wire.begin(wire_port);
-  Wire.onReceive(execute_command);
+  Wire.begin();
+  pinMode(fire_pin, OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
-}
-
-void execute_command(int howMany) {
+  Wire.requestFrom(wire_port, 1);
   char command = Wire.read();
-  int duration = Wire.read();
   switch(command) {
     case 'F':
-      forward(duration); 
+      forward(); 
       break;
     case 'B':
-      backward(duration);
+      backward();
       break;
     case 'L':
-      right(duration);
+      right();
       break;
     case 'R':
-      left(duration);
+      left();
+      break;
+    case 'S':
+      brake();
+      break;
+    case 'X':
+      fire();
       break;
   }
+  Serial.println(command);
+  delay(100); // we want to give time for the sonars to get a full cycle or something
 }
+
+void forward(){
+  motor_front->run(FORWARD);
+  motor_back->run(FORWARD);
+}
+void backward(){
+  motor_front->run(BACKWARD);
+  motor_back->run(BACKWARD);
+}
+void right(){
+  motor_front->run(FORWARD);
+  motor_back->run(BACKWARD);
+}
+void left(){
+  motor_front->run(BACKWARD);
+  motor_back->run(FORWARD);
+}
+void brake(){
+  motor_front->run(RELEASE);
+  motor_back->run(RELEASE);
+}
+void fire(){
+  digitalWrite(fire_pin, HIGH);
+  delay(100);
+  digitalWrite(fire_pin, LOW);
+}
+
--- Production/sensor_control/sensor_control.ino
+++ Production/sensor_control/sensor_control.ino
@@ -9,13 +9,16 @@
  * Right_Up A3
  */
 
-#define SONAR_NUM     5 // Number or sensors.
-#define MAX_DISTANCE 500 // Max distance in cm.
+#define SONAR_NUM 5 // Number or sensors.
+#define MAX_DISTANCE 300 // Max distance in cm.
 #define PING_INTERVAL 100 // Milliseconds between pings.
+#define wire_port 0x20 //Slave address
 
 unsigned long pingTimer[SONAR_NUM]; // When each pings.
 unsigned int cm[SONAR_NUM]; // Store ping distances.
+unsigned int ir[4]; // Store IR readings
 uint8_t currentSensor = 0; // Which sensor is active.
+char command = 'F'; //Always move foward...
      
 NewPing sonar[SONAR_NUM] = { // Sensor object array. Trigger,Echo (PWM)
   NewPing(13, 11, MAX_DISTANCE),
@@ -25,8 +28,14 @@ NewPing sonar[SONAR_NUM] = { // Sensor object array. Trigger,Echo (PWM)
   NewPing(13, 5, MAX_DISTANCE),
 };
 
+void echo_check();
+void react();
+void send_command();
+void check_ir();
+
 void setup() {
-  Wire.begin(); // join i2c bus (address optional for master)
+  Wire.begin(wire_port); // join i2c bus (address optional for master)
+  Wire.onRequest(send_command);
   pingTimer[0] = millis() + 75; // First ping start in ms.
   for (uint8_t i = 1; i < SONAR_NUM; i++){
     pingTimer[i] = pingTimer[i - 1] + PING_INTERVAL;
@@ -34,45 +43,57 @@ void setup() {
   Serial.begin(9600);
 }
 
-void echoCheck() { // If ping echo, set distance to array.
-  if (sonar[currentSensor].check_timer())
-    cm[currentSensor] = sonar[currentSensor].ping_result / US_ROUNDTRIP_CM;
-}
-
-void react() {
-  int duration = 1500;
-  char direct = 'F';
-  if (cm[2] < 10){
-    direct = 'B';
-  }
-  if (cm[1] < 20){
-    direct = 'R';
-  } else if (cm[3] < 20){
-    direct = 'L';
-  }
-  Serial.print(cm[0]);Serial.print(" ");
-  Serial.print(cm[1]);Serial.print(" ");
-  Serial.print(cm[2]);Serial.print(" ");
-  Serial.print(cm[3]);Serial.print(" ");
-  Serial.print(cm[4]);Serial.print(" ");
-  Wire.beginTransmission(8); // transmit to motor driver
-  Wire.write(direct);        // sends direction
-  Wire.write(duration);      // sends duration
-  Wire.endTransmission();    // stop transmitting
-}
 
 
 void loop() {
   for (uint8_t i = 0; i < SONAR_NUM; i++) {
     if (millis() >= pingTimer[i]) {
       pingTimer[i] += PING_INTERVAL * SONAR_NUM;
-      if (i == 0 && currentSensor == SONAR_NUM - 1)
-        react(); // Do something with results.
+      if (i == 0 && currentSensor == SONAR_NUM - 1){
+        check_ir();
+        react();
+      }
       sonar[currentSensor].timer_stop();
       currentSensor = i;
       cm[currentSensor] = 0;
-      sonar[currentSensor].ping_timer(echoCheck);
+      sonar[currentSensor].ping_timer(echo_check);
         }
       }
       // The rest of your code would go here.
     }
+
+void echo_check() { // If ping echo, set distance to array.
+  if (sonar[currentSensor].check_timer())
+    cm[currentSensor] = sonar[currentSensor].ping_result / US_ROUNDTRIP_CM;
+}
+
+void react(){
+  /*
+   * This is the code where logic for the next move is based. To determine the bot's next move, simply alter global variable 
+   * 'command'
+   * Don't ask me how this will work
+   * the ir array stores ir sensor readings
+   * cm stores sonar sensor readings
+   * heh
+   * enjoy
+   * Here are the commands:
+   * F - forward
+   * B - backward
+   * L - left
+   * R - right
+   * S - brake
+   * X - fire
+   */
+}
+
+void check_ir(){
+  ir[0] = analogRead(0);
+  ir[1] = analogRead(1);
+  ir[2] = analogRead(2);
+  ir[3] = analogRead(3);
+}
+
+void send_command(){
+  Wire.write(command);
+}
+
